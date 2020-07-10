3702c9b888f80ee7201f1c2e0885b739c5b473d5
unknown <snehanimbare15@gmail.com>
2020-07-10 22:43:11 +0530

    Doublet Combination of Coin

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 read -p "How many Times you want to Flip the Coin:" Limit
-declare -A Singlet
+declare -A Doublet
 
 for (( i=0; i<Limit; i++ ))
 do
-	num=$((RANDOM%2))
+	num=$((RANDOM%4))
 	if [[ $num -eq 0 ]]
      	then
-		Singlet[$i]=`echo H`
-		((count_H++))
-     	else
-        	Singlet[$i]=`echo T`
-		((count_T++))
+		Doublet[$i]=`echo HH`
+		((count_HH++))
+     	elif [[ $num -eq 1 ]]
+	then
+        	Doublet[$i]=`echo TH`
+		((count_TH++))
+	elif [[ $num -eq 2 ]]
+	then
+		Doublet[$i]=`echo HT`
+                ((count_HT++))
+	 else
+                Doublet[$i]=`echo TT`
+                ((count_TT++))
 	fi
 done
-echo "Singlet Directory : "${Singlet[@]}
-echo "Head have been faced $count_H times"
-echo "Tail have been faced $count_T times"
-echo "Head Percentage=" $(($count_H*100/$Limit))
-echo "Tail Percentage=" $(($count_T*100/$Limit))
+echo "Doublet Directory : "${Doublet[@]}
+echo "HH have been faced $count_HH times"
+echo "HT have been faced $count_HT times"
+echo "TH have been faced $count_TH times"
+echo "TT have been faced $count_TT times"
+echo "HH Percentage=" $(($count_HH*100/$Limit))
+echo "TH Percentage=" $(($count_TH*100/$Limit))
+echo "HT Percentage=" $(($count_HT*100/$Limit))
+echo "TT Percentage=" $(($count_TT*100/$Limit))
